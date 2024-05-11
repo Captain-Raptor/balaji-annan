43717a9e4589faf0774c5d7d0aa60c457a02e559
Captain-Raptor <160110210+Captain-Raptor@users.noreply.github.com>
2024-05-11 12:50:24 +0530

    backend

---
+++ backend/server.py
@@ -0,0 +1,16 @@
+from flask import Flask, jsonify
+from flask_restful import Resource, Api
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)  # Add this line to enable CORS
+api = Api(app)
+
+class Status(Resource):
+    def get(self):
+        return jsonify({'status': 'connected', 'message': 'hi'})
+
+api.add_resource(Status, '/')
+
+if __name__ == '__main__':
+    app.run(debug=True,port=80,host="0.0.0.0")
---
+++ front/src/App.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const [status, setStatus] = useState(null);
+
+  useEffect(() => {
+    fetch('http://127.0.0.1:5000/')
+      .then(response => response.json())
+      .then(data => {
+        setStatus(data.status);
+      })
+      .catch(error => {
+        console.error('Error fetching status:', error);
+        setStatus('not connected',error);
+      });
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        {status ? (
+          <p>Status: {status}</p>
+        ) : (
+          <p>Loading...</p>
+        )}
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
